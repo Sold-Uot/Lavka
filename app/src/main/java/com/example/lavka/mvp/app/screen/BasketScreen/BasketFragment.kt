de6716ce160c22c599045b179ed6b579ab4c6aad
Sold-Uot <66266458+Sold-Uot@users.noreply.github.com>
2022-08-23 17:00:07 +0300

    testbrunch2

--- app/src/main/java/com/example/lavka/mvp/app/screen/BasketScreen/BasketFragment.kt
+++ app/src/main/java/com/example/lavka/mvp/app/screen/BasketScreen/BasketFragment.kt
@@ -27,6 +27,7 @@ class BasketFragment : Fragment(R.layout.fragment_basket), Contract.view {
 
     val mPresenter: Presenter = Presenter(this)
 
+    val s = 12
     companion object {
         lateinit var byButton: Button
          fun RxLoadBasketArray(): Observable<ArrayList<ProductCartModel>> {
